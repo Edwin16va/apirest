e30f03ac5fdab42319b7d41cab2e1c0d04c834fc
Edwin Valenzuela <valenzuelaed16@gmail.com>
2022-06-23 13:08:11 -0500

    Nuevo

--- connection/Connection.php
+++ connection/Connection.php
@@ -5,7 +5,7 @@
 
             parent::__construct('localhost', 'root', '', 'api_rest');
            // parent::__construct('sql312.epizy.com', 'epiz_31992964', 'sDMVe5u5tAYBt06', 'epiz_31992964_api_rest');
-           //probando github
+           //probando github hola a todos
 
             $this->set_charset('utf8');
             $this->connect_error == NULL ? 'Conexión exítosa a la DB' : die('Error al conectarse a la BD');
