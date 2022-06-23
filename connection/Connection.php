2bb3eb42c824d66bd0366b31a5680698ea1f0193
Edwin Valenzuela <valenzuelaed16@gmail.com>
2022-06-23 15:12:23 -0500

    update

--- connection/Connection.php
+++ connection/Connection.php
@@ -3,8 +3,11 @@
     class Connection extends Mysqli {
         function __construct() {
 
-            parent::__construct('localhost', 'root', '', 'api_rest');
-           // parent::__construct('sql312.epizy.com', 'epiz_31992964', 'sDMVe5u5tAYBt06', 'epiz_31992964_api_rest');
+           // parent::__construct('localhost', 'root', '', 'api_rest');
+            parent::__construct('mysql-programacion.alwaysdata.net', 
+            '273767_pro', 
+            'Programacion_Avanzada',
+             'programacion_api');
            //probando github hola a todos
 
             $this->set_charset('utf8');
